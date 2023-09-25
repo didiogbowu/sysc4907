bf398d502626b6c64bb4d381b077da8d5a89d6a6
didiogbowu <didikiyeogbowu@cmail.carleton.ca>
2023-09-25 16:28:57 -0400

    Added Program Selection and Base Planning Tool

--- myDegree_v1/mydegree/forms.py
+++ myDegree_v1/mydegree/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, SelectField
 from wtforms.validators import DataRequired
+
+PROGRAM_CHOICES = [("cse", "Systems and Computer Engineering"), ("se", "Software Engineering")]
 
 class SubmitButtonForm(FlaskForm):
     submit = SubmitField('Submit', validators=[DataRequired()])
@@ -8,3 +10,7 @@ class SubmitButtonForm(FlaskForm):
 class TextFieldForm(FlaskForm):
     name_of_course = StringField('Name of Course', validators=[DataRequired()])
     submit = SubmitField('Add')
+    
+class SelectProgramForm(FlaskForm):
+    name_of_course = SelectField('Select your engineering program', validators=[DataRequired()], choices=PROGRAM_CHOICES, coerce=str, option_widget=None, validate_choice=True)
+    submit = SubmitField('Go')
--- myDegree_v1/mydegree/routes.py
+++ myDegree_v1/mydegree/routes.py
@@ -1,175 +1,19 @@
-from flask import render_template, redirect, url_for, request, flash
+import os
+from flask import render_template, json, redirect, url_for, request, flash, current_app
 from mydegree import app
-from mydegree.forms import SubmitButtonForm, TextFieldForm
+from mydegree.forms import SubmitButtonForm, TextFieldForm, SelectProgramForm, PROGRAM_CHOICES
 from mydegree.data_structures import all_timetables
 from mydegree.render_timetable import course_height, course_mt, course_ml
 
-list_of_names = ['SYSC 3006', 'SYSC 4001', 'ELEC 2607', 'ELEC 2501']
+list_of_names = []
 
 program = "Systems and Computer Engineering"
 
-courses = [
-    [{
-        'course_code':'ECOR 1042',
-        'course_title': 'Data Management'
-    },
+with app.app_context():
+    file = os.path.join(current_app.static_folder, 'data', 'cse.json')
 
-    {
-        'course_code':'MATH 1004',
-        'course_title': 'Calculus for Eng. Students'
-    },
-
-    {
-        'course_code':'CHEM 1101',
-        'course_title': 'Chemistry for Eng. Students'
-    },
-
-    {
-        'course_code':'SYSC 2006',
-        'course_title': 'Foundation of Imperative Programming'
-    }],
-
-    [{
-        'course_code':'MATH 1104',
-        'course_title': 'Linear Algebra for Eng. Students'
-    },
-    
-    {
-        'course_code':'PHYS 1004',
-        'course_title': 'Introductory Electromagnetism & Wave Motion'
-    },
-
-    {
-        'course_code':'Elective',
-        'course_title': 'Complementary Studies Elective'
-    },
-
-    {
-        'course_code':'SYSC 2310',
-        'course_title': 'Introduction to Digital Systems'
-    }],
-
-    [{
-        'course_code':'MATH 1005',
-        'course_title': 'Differential Equations & Infinite Series for Eng. Students'
-    },
-    
-    {
-        'course_code':'PHYS 1007',
-        'course_title': 'University Physics I'
-    },
-
-    {
-        'course_code':'SYSC 2004',
-        'course_title': 'O.O. Software Development'
-    },
-    
-    {
-        'course_code':'Elective',
-        'course_title': 'Basic Science Elective'
-    }],
-
-    [{
-        'course_code':'CCDP 2100',
-        'course_title': 'Communication Skills for Eng. Students'
-    },
-    
-    {
-        'course_code':'PHYS 1004',
-        'course_title': 'Introductory Electromagnetism & Wave Motion'
-    },
-
-    {
-        'course_code':'SYSC 2510',
-        'course_title': 'Probability, Statistics & Random Processes'
-    },
-
-    {
-        'course_code':'ECOR 2050',
-        'course_title': 'Design & Analysis of Engineering Experiment'
-    }],
-
-    [{
-        'course_code':'MATH 2004',
-        'course_title': 'Multivariable Calculus for Eng. Students'
-    },
-    
-    {
-        'course_code':'SYSC 2100',
-        'course_title': 'Algorithms & Data Structures'
-    },
-
-    {
-        'course_code':'SYSC 3600',
-        'course_title': 'Systems & Simulation'
-    },
-
-    {
-        'course_code':'SYSC 3320',
-        'course_title': 'Computer Systems Design'
-    }],
-
-    [{
-        'course_code':'SYSC 3006',
-        'course_title': 'Computer Organization'
-    },
-    
-    {
-        'course_code':'SYSC 4001',
-        'course_title': 'Operating Systems'
-    },
-    
-    {
-        'course_code':'ELEC 2501',
-        'course_title': 'Circuits & Signals'
-    },
-
-    {
-        'course_code':'ELEC 2607',
-        'course_title': 'Switching Circuits'
-    }],
-
-    [{
-        'course_code':'MATH 3705',
-        'course_title': 'Mathematical Methods'
-    },
-    
-    {
-        'course_code':'ECOR 3800',
-        'course_title': 'Engineering Economics'
-    },
-
-    {
-        'course_code':'SYSC 3501',
-        'course_title': 'Communication Theory'
-    },
-
-    {
-        'course_code':'SYSC 2320',
-        'course_title': 'Computer Organization & Architecture'
-    }],
-
-    [{
-        'course_code':'MATH 1800',
-        'course_title': 'Introduction to Mathematical Reasoning'
-    },
-    
-    {
-        'course_code':'SYSC 4602',
-        'course_title': 'Computer Communications'
-    },
-
-    {
-        'course_code':'Elective',
-        'course_title': 'Complementary Studies Elective'
-    },
-
-    {
-        'course_code':'SYSC 3020',
-        'course_title': 'Introduction to Software Engineering'
-    }]
-]
-
+    with open(file) as f:
+        courses = json.load(f)
 
 @app.route("/")
 @app.route("/input_page", methods=['GET', 'POST'])
@@ -209,3 +53,23 @@ def home():
         #flash(f'{list_of_names}', 'success')
         return redirect(url_for('result'))
     return render_template('home.html', courses = courses, title = program, submit_button = submit_button, str = str)
+    
+@app.route("/select_program", methods=['GET', 'POST'])
+def select_program():
+    select_form = SelectProgramForm()
+    
+    if select_form.validate_on_submit():
+        flash("yeah", "success")
+        
+        """
+        program = dict(PROGRAM_CHOICES).get(select_form.name_of_course.data)
+        
+        with app.app_context():
+            file = os.path.join(current_app.static_folder, 'data', str(select_form.name_of_course.data) + '.json')
+
+            with open(file) as f:
+                courses = json.load(f)
+        """
+        return redirect(url_for('home'))
+    
+    return render_template('select_program.html', select_form = select_form);
