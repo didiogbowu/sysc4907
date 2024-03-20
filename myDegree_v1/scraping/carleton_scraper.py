f981f08b063900f3973f0adc4974a73bb113ce54
Henry <henrylin4@cmail.carleton.ca>
2024-03-20 01:38:19 -0400

    Scrapers for course info

---
+++ myDegree_v1/scraping/carleton_scraper.py
@@ -0,0 +1,124 @@
+import http.client
+import os
+import time
+from datetime import datetime
+from pathlib import Path
+
+import requests
+
+import course_info
+# import program_tree
+
+DEPTS = {"SYSC", "ELEC", "ECOR", "CCDP", "COMP",
+         "MATH", "PHYS", "CHEM", "BIOL", "CIVE",
+         "AERO", "ENVE", "MECH", "MAAE", "SREE"}
+PROGRAMS = ["Computer Systems", "Software", "Communications"]
+
+DEPT_COURSES_URL = "https://calendar.carleton.ca/undergrad/courses/"
+CURRENT_CALENDAR_URL = "https://calendar.carleton.ca/undergrad/undergradprograms/engineering/"
+OLD_CALENDAR_URL = "https://calendar.carleton.ca/calendars/YEAR/undergrad/undergradprograms/engineering/"
+
+DEPT_DIR = "departments"
+DEPT_HTML_DIR = f"{DEPT_DIR}/raw_html"
+PROGRAM_DIR = "programs"
+PROGRAM_HTML_DIR = f"{PROGRAM_DIR}/raw_html"
+
+
+def fetch_and_store(url: str, destination: str, replace=False) -> None:
+    """
+    Fetches the page at the given URL and stores it as a html file
+    at the given file path. Optionally may replace the file if it
+    already exists (skips by default).
+    :param url: The url to fetch
+    :param destination: The destination file path to store it in
+    :param replace: True to replace existing file, False to ignore
+    :return: None
+    """
+    if not os.path.isfile(destination) or replace:
+
+        max_retries = 5
+        for attempt in range(max_retries):
+            try:
+                page = requests.get(url, headers={
+                    "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
+                })  # Use random user agent to mitigate blocked requests
+            except (requests.exceptions.ConnectionError, http.client.RemoteDisconnected) as e:
+                time.sleep(1.5 ** attempt)  # Exponential backoff if fetch fails
+                if attempt >= max_retries - 1:
+                    print(f"Max retries exceeded. {destination} not acquired")
+                else:
+                    print(f"Connection failed, retrying #{attempt}")
+                continue
+
+            Path(destination).parent.mkdir(parents=True, exist_ok=True)
+            with open(destination, "w", encoding="utf-8") as file:
+                file.write(page.text)
+            break
+
+
+def scrape_dept_courses() -> None:
+    """
+    Fetches the html for all departments and stores them in the DEPT_HTML_DIR.
+    Files already present will not be fetched again.
+    :return: None
+    """
+    for dept in DEPTS:
+        dept_file = f"{DEPT_HTML_DIR}/{dept}.html"
+        dept_url = DEPT_COURSES_URL + dept
+        fetch_and_store(dept_url, dept_file)
+
+
+def scrape_program_trees() -> None:
+    """
+    Fetches the html for the last five years from Carleton's website, storing
+    the files in the directory specified by global PROGRAM_HTML_DIR.
+    Files already present will not be fetched again.
+    :return: None
+    """
+    years = get_catalog_years(5)
+    for year in years:
+        catalog_file = f"{PROGRAM_HTML_DIR}/{catalog_filename("", year)}.html"
+        if year == years[-1]:
+            calendar_url = CURRENT_CALENDAR_URL
+        else:
+            calendar_url = f"{OLD_CALENDAR_URL.replace("YEAR", year)}"
+        fetch_and_store(calendar_url, catalog_file)
+
+
+def catalog_filename(program_name: str, catalog_year: str) -> str:
+    """
+    Return a filename generated based on the given program and catalog year.
+    Filename is generated using the convention Program_Name_Catalog_Year, and does
+    *not* include file suffix (e.g., .html, .json). If program name is empty,
+    uses "programs" in its place.
+    :param program_name: The program name, or an empty string
+    :param catalog_year: The catalog year
+    :return: The conventional filename for the given parameters
+    """
+    if program_name:
+        return f"{program_name.replace(" ", "_")}_{catalog_year}"
+    else:
+        return f"programs_{catalog_year}"
+
+
+def get_catalog_years(years_to_keep: int) -> list[str]:
+    """
+    Return a list of catalog years (strings in format 20xx-20xx) including
+    the current year and the n - 1 preceding years, in ascending order.
+    Catalog year is assumed to change after June of every year.
+    :param years_to_keep: The number of years back to include
+    :return: A list of catalog years
+    """
+    today = datetime.now()
+    start_years = (list(range(today.year - years_to_keep, today.year)) if today.month <= 6
+                   else list(range(today.year - years_to_keep + 1, today.year + 1)))
+    return [f"{year}-{year + 1}" for year in start_years]
+
+
+if __name__ == "__main__":
+    scrape_program_trees()
+    scrape_dept_courses()
+    directory = DEPT_HTML_DIR
+    for dept_file in [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]:
+        with open(f"{directory}/{dept_file}", "r", encoding="utf-8") as source:
+            print(course_info.scrape_courses(source.read()))
---
+++ myDegree_v1/scraping/course_info.py
@@ -0,0 +1,239 @@
+import re
+import unicodedata
+from bs4 import BeautifulSoup
+import json
+
+from pyparsing import Regex, nested_expr
+
+COURSE_REGEX = r"c?[A-Z]{4} \d{4}"  # Matches course codes. A 'c' prefix is used by parser to indicate concurrent
+OR_REGEX = f"({COURSE_REGEX},? *)+( *or *{COURSE_REGEX})+"
+# Matches strings like:
+# - COURSE, COURSE, or COURSE
+# - COURSE or COURSE
+# accounting for duplicated spaces, oxford comma, and any number of courses
+
+
+def isolate_groups(sample: str) -> (str, dict[str, str]):
+    """
+    Isolate top-level parenthesized groupings of courses from input sample, returning the string with
+    groupings replaced by tokens XXXX 000N (to simulate a course code) and a dictionary of tokens and
+    their corresponding groupings.
+    :param sample: The string to extract groupings from
+    :return: The tokenized string and a dictionary of token-group pairs
+    """
+    groups = {}
+    stack = []
+    for i in reversed(range(len(sample))):  # Work in reverse to avoid shifting string indices
+        char = sample[i]
+        if char == ")":
+            stack.append(i)
+        elif char == "(":
+            if len(stack) > 1:
+                stack.pop()
+            elif len(stack) == 1:
+                end_index = stack.pop()
+                start_index = i
+                group = sample[start_index:end_index + 1]
+                if len(re.findall(COURSE_REGEX, group)) > 1:
+                    key = f"XXXX {len(groups):>04}"
+                    groups[key] = group
+                    sample = sample[:start_index] + key + sample[end_index + 1:]  # Replace group with key
+            else:
+                print(f"'{sample}'", ": Opening parenthesis without closing parenthesis")
+    if stack:
+        print(f"'{sample}'", ": Closing parenthesis without opening parenthesis")
+    return sample, groups
+
+
+def scrape_courses(page: str):
+    soup = BeautifulSoup(page, "html.parser")
+    courseblocks = soup.findAll(class_="courseblock")
+    courses = []
+    for course in courseblocks:
+        desc = course.find(class_="coursedescadditional")
+        if desc is None:
+            continue
+
+        code = unicodedata.normalize("NFKD", course.find(class_="courseblockcode").get_text())
+        entry = {"code": code,
+                 "blurb": course.contents[3].get_text().strip(),
+                 "precludes": None,
+                 "prerequisites": None,
+                 "schedule": None}
+        courses.append(entry)
+
+        for line_break in desc.findAll("br"):
+            line_break.replaceWith("\n")
+        desc_lines = unicodedata.normalize("NFKD", desc.get_text()).split("\n")
+
+        for line in desc_lines:
+            if "Preclude" in line:
+                entry["precludes"] = re.findall(COURSE_REGEX, line)
+            elif "Prerequisite" in line:
+                entry["prerequisites"] = prerequisite_tree(line)
+            elif "Lecture" in line:
+                entry["schedule"] = line
+
+    return courses
+
+
+def parse_prerequisites(req_text: str) -> (str, list):
+    """
+    Parse the given plain English prerequisites and return
+     - a string with courses separated by '&' or '|', e.g. 'SYSC 2006&(SYSC 2320|SYSC 3006)'
+     - a list of additional requirements, e.g. ['min grade', 'year standing']
+    :param req_text: Prerequisites in plain English
+    :return: prerequisites in boolean format, list of additional conditions
+    """
+    # Determine if special requirements are present
+    # Regex pattern to match : brief description of condition
+    special_requirements = {
+        r"year [(status)(standing)]": "year standing",
+        r"[(Engineering)(program)(enrol)]": "program restriction",
+        "permission": "department permission",
+        "grade": "minimum grade",
+        "recommended background": "recommended background",
+        "CGPA": "minimum CGPA",
+        "may not be taken concurrently": "disallowed courses"
+    }
+    do_not_parse = ["recommended background", "disallowed courses"]
+    conditions = []
+    for pattern in special_requirements:
+        keyword = re.search(pattern, req_text)
+        if keyword:
+            descriptor = special_requirements[pattern]
+            conditions.append(descriptor)
+            if descriptor in do_not_parse:
+                req_text = req_text[:keyword.start()]  # Avoid parsing recommended or disallowed courses
+
+    req_text, groups = isolate_groups(req_text)
+
+    # extract OR groups first
+    disjoint_blocks = list(re.finditer(OR_REGEX, req_text))
+    offset = 0
+    reqs = {}
+    for block in disjoint_blocks:
+        req_text = req_text[:block.start() - offset] + req_text[block.end() - offset:]
+        offset += len(block.group())
+        courses = re.findall(COURSE_REGEX, block.group())
+        reqs[block.end() - offset] = f"({'|'.join(courses)})"
+
+    # Remaining courses are assumed to be all required
+    for remaining in re.finditer(COURSE_REGEX, req_text):
+        reqs[remaining.end()] = remaining.group()
+
+    # Find concurrent courses and add 'c' to them
+    concurrents = [match.start() for match in re.finditer("concurrent", req_text)]
+    indices = list(reqs.keys())
+    if not indices:
+        return "", conditions
+
+    indices.sort()
+    for c_pos in concurrents:
+        closest_course_i = indices[0]
+        for i in reversed(indices):
+            closest_course_i = i
+            if i < c_pos:
+                break
+        for course in reversed(list(re.finditer(COURSE_REGEX, reqs[closest_course_i]))):
+            text = reqs[closest_course_i]
+            reqs[closest_course_i] = text[:course.start()] + "c" + text[course.start():]
+
+    # Search text between each course for patterns that indicate separation between groups
+    separator_patterns = [
+        " or ",  # OR groups have already been extracted, remaining or indicates separation
+        r"\. "   # end of sentence
+    ]
+    req_groups = [[]]
+    for i in range(len(indices) - 1):
+        start = indices[i]
+        end = indices[i + 1]
+        filler = req_text[start:end]
+        req_groups[-1].append(reqs[start])
+        for pattern in separator_patterns:
+            separator = re.search(pattern, filler)
+            if separator:
+                req_groups.append([])
+        if i == len(indices) - 2:
+            req_groups[-1].append(reqs[end])
+
+    if len(req_groups) > 1:
+        joinstrings = ["&".join(group) for group in req_groups]
+        reqstring = "|".join([string for string in joinstrings if string])
+    else:
+        reqstring = "&".join(reqs.values())
+
+    for token in groups:
+        group, _ = parse_prerequisites(groups[token][1:-1])  # Strip surrounding parentheses, then recurse
+        replace_index = reqstring.find(token)
+        if reqstring[replace_index - 1] == "c":
+            # Group becomes concurrent
+            group = re.sub(COURSE_REGEX, lambda course_code: f"c{course_code.group()}", group)
+            reqstring = reqstring[:replace_index - 1] + group + reqstring[replace_index + len(token):]
+            reqstring = f"({reqstring})"
+        else:
+            reqstring = reqstring.replace(token, group)
+
+    return reqstring, conditions
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.children = []
+
+    def add_child(self, node):
+        self.children.append(node)
+
+    def as_dict(self):
+        return {"data": self.data, "children": [child.as_dict() for child in self.children]}
+
+
+def build_tree(stack: list, elements: list):
+    if elements:
+        for i in range(len(elements)):
+            if elements[i] == "&" or elements[i] == "|":
+                # Operator node
+                root = Node(elements[i])
+                root.add_child(stack.pop())
+                stack.append(root)
+            elif type(elements[i]) is list:
+                # Compound expression
+                subtree = build_tree(stack, elements[i])
+                stack.append(subtree)
+            else:
+                # Operand node
+                leaf = Node(elements[i])
+                stack.append(leaf)
+        root = stack.pop()
+        while len(stack) > 0:
+            leaf = root
+            root = stack.pop()
+            root.add_child(leaf)
+        return root
+    return Node(None)
+
+
+def prerequisite_tree(req_text: str) -> dict:
+    reqstring, conditions = parse_prerequisites(req_text)
+
+    word = Regex(COURSE_REGEX)
+    expr = nested_expr("(", ")", content=word | "|" | "&")
+    if reqstring:
+        if reqstring[0] == "(" and reqstring[-1] == ")":
+            parse = expr.parse_string(reqstring)[0].asList()
+        else:
+            parse = expr.parse_string(f"({reqstring})")[0].asList()
+    else:
+        parse = []
+
+    tree = build_tree([], parse).as_dict()
+    if conditions:
+        tree["conditions"] = conditions
+        tree["text"] = req_text
+    return tree
+
+
+if __name__ == "__main__":
+    with open("departments/raw_html/AERO.html", "r", encoding="utf-8") as f:
+        print(json.dumps(scrape_courses(f.read()), indent=2))
