319907a21d1d5ddc5681709dd73866dc0d4ee526
didiogbowu <didikiyeogbowu@cmail.carleton.ca>
2024-04-06 22:22:38 -0400

    Updated Database with Courses Without Meeting Times

--- myDegree_v1/mydegree/models.py
+++ myDegree_v1/mydegree/models.py
@@ -5,11 +5,11 @@ class CourseData(db.Model):
     course_code = db.Column(db.String(15), nullable=False)
     course_title = db.Column(db.String(120), nullable=False)
     semester = db.Column(db.String(15), nullable=False)
-    first_day = db.Column(db.Integer, nullable=False)
+    first_day = db.Column(db.Integer, nullable=True)
     second_day = db.Column(db.Integer, nullable=True)
-    week_frequency = db.Column(db.Integer, nullable=False)
-    start_time = db.Column(db.Integer, nullable=False)
-    end_time = db.Column(db.Integer, nullable=False)
+    week_frequency = db.Column(db.Integer, nullable=True)
+    start_time = db.Column(db.Integer, nullable=True)
+    end_time = db.Column(db.Integer, nullable=True)
 
     def __repr__(self):
         return f"\n{self.course_code}\n{self.course_title}"
