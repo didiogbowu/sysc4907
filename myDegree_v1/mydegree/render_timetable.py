d9df0b632ac30040e73a0d46adca0895a4437f92
didiogbowu <didikiyeogbowu@cmail.carleton.ca>
2024-04-14 01:49:34 -0400

    Documentation Complete

--- myDegree_v1/mydegree/render_timetable.py
+++ myDegree_v1/mydegree/render_timetable.py
@@ -1,11 +1,9 @@
-HRS_TO_MINS = 60
-TIMETABLE_ST = 8 * HRS_TO_MINS
+HRS_TO_MINS = 60 # The number of minutes in one hour
+TIMETABLE_ST = 8 * HRS_TO_MINS # This represents 8 o'clock in the morning
 
-COURSE_WIDTH = 100
-HEIGHT = 30
-HEIGHT_REF = 30
-B_HEIGHT = 70
-B_WIDTH = 50
+COURSE_WIDTH = 100 # The CSS width in px of each time slot
+HEIGHT = 30 # The CSS height in px
+HEIGHT_REF = 30 # This was used for scaling the height. HEIGHT divided by HEIGHT_REF gives the scale
 
 def course_height(end_time, start_time):
     return (HEIGHT * ((end_time - start_time) / HEIGHT_REF))
